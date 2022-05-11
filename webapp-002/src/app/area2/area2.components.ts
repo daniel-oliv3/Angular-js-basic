fb1f12ecf256cb0196c1545fb5d20deb33453d6b
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-11 17:51:04 -0300

    Update

--- webapp-002/src/app/area2/area2.components.ts
+++ webapp-002/src/app/area2/area2.components.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-area2',
-  templateUrl: './area2.components.html'
+  templateUrl: './area2.components.html',
+  styleUrls: ['./area2.components.css']
 })
 
 export class area2{}
