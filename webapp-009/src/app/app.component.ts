393f1efa759b38bf69d0acba537ef54580d9eee6
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-12 11:54:22 -0300

    Update

--- webapp-009/src/app/app.component.ts
+++ webapp-009/src/app/app.component.ts
@@ -3,7 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [
+    `
+      .cor {
+        color: green;
+      }
+
+      .cor2 {
+        color: lightgray;
+      }
+
+    `
+  ]
 })
 export class AppComponent {
   title = 'webapp-009';
