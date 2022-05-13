a516ed7997f8d452e723a4b09a952dec8861afcc
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-13 11:56:11 -0300

    Update

--- webapp-013/src/app/app.component.ts
+++ webapp-013/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'webapp-013';
+  //title = 'webapp-013';
+  inativo: boolean = true;
+
+  constructor(){
+    this.colocarAtivo();
+  }
+
+  colocarAtivo(){
+    setTimeout(() => {
+      this.inativo = false;
+    }, 5000);
+  }
 }
