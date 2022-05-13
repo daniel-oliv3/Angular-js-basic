318c8bff12bc0f978a4b4b1064b806878f3cd570
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-13 19:19:12 -0300

    Up

--- webapp-014/src/app/app.component.ts
+++ webapp-014/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'webapp-014';
+  //title = 'webapp-014';
+  inativo: boolean = true;
+  texto: string = 'Texto do placholder';
+
+  constructor(){
+    this.colocarAtivo();
+  }
+
+  colocarAtivo(){
+    setTimeout(() => {
+      this.inativo = false;
+      this.texto = "Texto alterado!";
+    }, 5000);
+  }
 }
