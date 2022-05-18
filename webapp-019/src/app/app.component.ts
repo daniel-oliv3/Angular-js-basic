c8d42f9f3d7d5defdad7dd1375aa943ec69ee201
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-18 14:16:03 -0300

    Update

--- webapp-019/src/app/app.component.ts
+++ webapp-019/src/app/app.component.ts
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //...
-  
-}
+
+  apresentar: boolean = false;
+  texto_botao: string = 'Apresentar';
+
+  //Adiciona ou remove o paragrafo.
+  btn_Click(){
+    if(this.apresentar){
+      this.apresentar = false;
+      this.texto_botao = 'Apresentar';
+    }else {
+        this.apresentar = true;
+        this.texto_botao = 'Remover';
+      }
+    }
+  }
+
+
+
+
+
