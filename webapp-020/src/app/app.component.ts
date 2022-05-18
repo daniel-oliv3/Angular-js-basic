f8c20d990bf33c0b739acf82f79e07a16c518cac
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-18 14:54:29 -0300

    Update

--- webapp-020/src/app/app.component.ts
+++ webapp-020/src/app/app.component.ts
@@ -6,7 +6,30 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   //...
-  
-}
+
+  apresentar: boolean = false;
+
+  btn_Click(){
+    if(this.apresentar){
+      this.apresentar = false;
+    }else{
+      this.apresentar = true;
+    }
+  }
+
+
+  }
+
+
+
+
+
+
+
+
+
+
