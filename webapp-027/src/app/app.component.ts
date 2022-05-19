d1e7036fb03ea3747286563b31dca64e1a3d7f2a
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-19 13:28:24 -0300

    Update

--- webapp-027/src/app/app.component.ts
+++ webapp-027/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webapp-027';
+
+//variaveis que controlam a visibilidade dos componentes
+  loja_visivel: boolean = true;
+  stock_visivel: boolean = false;
+
+
+//Função MostrarLoja, apresenta a loja e remove o stock
+  MostrarLoja(){
+    this.loja_visivel = true;
+    this.stock_visivel = false;
+  }
+
+//Função MostrarStock, apresenta a stock e remove a loja
+  MostrarStock(){
+    this.loja_visivel = false;
+    this.stock_visivel = true;
+  }
+
 }
