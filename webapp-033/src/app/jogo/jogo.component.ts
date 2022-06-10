8816eea6d224935b221ee34ac61bfdf1aafc2a1a
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-10 13:46:31 -0300

    Update

--- webapp-033/src/app/jogo/jogo.component.ts
+++ webapp-033/src/app/jogo/jogo.component.ts
@@ -9,13 +9,33 @@ export class JogoComponent implements OnInit {
 
 
   /*Elementos que constituem a operação*/
-  valo1: number = 0;
-  valo2: number = 0;
+  valor1: number = 0;
+  valor2: number = 0;
   resultado: number = 0;
 
 
+  str_operacao: string = '';
+  valor_inserido: number = 0;
 
-  constructor() { }
+  /* */
+  constructor() {
+    this.CriaNovaOperacao();
+  }
+
+  /* Metodos */
+  CriaNovaOperacao(){
+    //cria aleatoriamente uma adição
+    this.valor1 = Math.floor(Math.random()*10);
+    this.valor2 = Math.floor(Math.random()*10);
+    this.resultado = this.valor1 + this.valor2;
+    this.str_operacao = this.valor1 + ' + ' + this.valor2 + " = ";
+  }
+
+
+
+
+
+
 
   ngOnInit(): void {
   }
